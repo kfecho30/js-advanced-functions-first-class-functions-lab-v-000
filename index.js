d16368d2a9bd0a80c96cbfd428fcd92ccc51a9d8
Kelsey Fecho <kfecho30@gmail.com>
2018-06-19 20:48:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,7 @@ const createFareMultiplier = function(mult){
 const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (arr, func) {
+  return arr.func();
+}
